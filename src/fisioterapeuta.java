d5a048e45d9548bed6758cc9bd33b39b662ec7ff
Lanaduti <reissalana5@gmail.com>
2026-06-27 16:37:11 -0300

    feat: corrigi herança e nome do arquivo Fisioterapeuta

--- src/fisioterapeuta.java
+++ src/fisioterapeuta.java
@@ -1,19 +1,14 @@
 public class Fisioterapeuta extends Profissional {
+    private String crefito;
 
-    private int sessoes;
-
-    public Fisioterapeuta(String nome, String cpf, String telefone, String dataNascimento, 
-                          String registro, double valor, int sessoes) {
-        super(nome, cpf, telefone, dataNascimento, registro, valor);
-        this.sessoes = sessoes;
+    public Fisioterapeuta(String nome, String registroProfissional, double valorConsulta, String crefito) {
+        // Passa os dados certos para o construtor do professor
+        super(nome, "fisioterapia", registroProfissional, valorConsulta);
+        this.crefito = crefito;
     }
 
     @Override
-    public void exibirResumo() {
-        System.out.println("--- Fisioterapeuta ---");
-        System.out.println("Nome: " + getNome());
-        System.out.println("Registro: " + getRegistroProfissional());
-        System.out.println("Sessões previstas: " + this.sessoes);
-        System.out.println("Valor da Consulta: R$" + getValorConsulta());
+    public String exibirResumo() {
+        return super.exibirResumo() + " | CREFITO: " + this.crefito;
     }
 }
