1fe96d0300790ab9e2f23dbe6a7635d951cc4811
alana reis <reissalana5@gmail.com>
2026-06-28 13:47:27 -0300

    Fix constructor comment in Nutricionista class

--- src/Nutricionista.java
+++ src/Nutricionista.java
@@ -2,7 +2,7 @@ public class Nutricionista extends Profissional {
     private String crn;
 
     public Nutricionista(String nome, String registroProfissional, double valorConsulta, String crn) {
-        // Passa os parâmetros certos para a classe do professor ("nutricao")
+
         super(nome, "nutricao", registroProfissional, valorConsulta);
         this.crn = crn;
     }
