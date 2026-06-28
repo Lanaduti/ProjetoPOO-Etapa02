2bbe8d35a2c75643a93fbf70f2ce87e21c111ae6
alana reis <reissalana5@gmail.com>
2026-06-28 12:31:01 -0300

    Clean up comments in ProfissionalNaoEncontradoException
    Removed comments regarding exception handling and constructor overload.

--- ProfissionalNaoEncontradoException.java
+++ ProfissionalNaoEncontradoException.java
@@ -1,5 +1,3 @@
-// R9: Exceção personalizada VERIFICADA (estende Exception => checked exception).
-// SOBRECARGA de construtor: um só com a mensagem e outro com mensagem + causa.
 public class ProfissionalNaoEncontradoException extends Exception {
 
     public ProfissionalNaoEncontradoException(String mensagem) {
